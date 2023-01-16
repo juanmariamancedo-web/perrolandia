c21f3a594d939cffc994087bd29daaad69921998
Juan María <47181406+JuanMa96@users.noreply.github.com>
2023-01-16 18:12:39 -0300

    Ahora las tarjetas se cargan luego de las imagenes

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -1,17 +1,11 @@
 export class Card extends React.Component{
     constructor(props){
         super(props);
+        
         this.state = {
-            imgUrl: ""
+            imgUrl: props.imgUrl !== undefined? props.imgUrl: ""
         }
         this.handleChange = this.handleChange.bind(this)
-    }
-
-
-    componentDidMount() {
-        axios(`https://dog.ceo/api/breed/${this.props.breed}/images/random`)
-        .then((res)=>{this.setState({imgUrl : res.data.message})})
-        .catch((err)=>{console.log(err)})
     }
 
     handleChange(){
@@ -20,7 +14,7 @@ export class Card extends React.Component{
         .catch((err)=>{console.log(err)})
     }
 
-    render(){
+    render(){      
         return(
             <div className="col col-sm-6 col-md-5">
             <article className="card  text-bg-dark h-100">
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import { CardsContainer } from "./components/CardsContainer.jsx";
 
-ReactDOM.createRoot(document.getElementById('rootCards')).render(<CardsContainer/>);
+Promise.all([
+    axios(`https://dog.ceo/api/breed/dalmatian/images/random`).then(res=>res.data.message),
+    axios(`https://dog.ceo/api/breed/pitbull/images/random`).then(res=>res.data.message),
+    axios(`https://dog.ceo/api/breed/akita/images/random`).then(res=>res.data.message),
+    axios(`https://dog.ceo/api/breed/husky/images/random`).then(res=>res.data.message)
+])
+.then((res)=>{
+    ReactDOM.createRoot(document.getElementById('rootCards')).render(<CardsContainer imgUrl={res}/>);
+})
+.catch((err)=>{console.log(err.message)})
